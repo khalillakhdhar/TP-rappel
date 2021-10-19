5e834a8744ecf5186888dfed51cee9fb7d7c5ca8
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-19 14:29:10 +0100

    boucles

--- range.py
+++ range.py
@@ -1,4 +1,10 @@
 for x in range(6):
     print(x)
+else:
+    print(" finished!")
 for x in range(2,5):
+    print(x)
+else:
+    print("second finished")
+for x in range(0,8,2):
     print(x)
---
+++ tpboucle.py
@@ -0,0 +1,8 @@
+x=-1
+while x<=0:
+    x=int(input("donner un entier"))
+print("la valeur positive est: %s"%(x))
+myList = [1, 2, 20, -4, 5, 12, 7, -8, 3, 10]
+for x in myList:
+    if x>0:
+        print(x)
